6dd9bb47227c90524b7e614f5bacb7a5465beb8e
kikalla <giorgi.kikadze@gmail.com>
2022-07-09 20:45:53 +0400

    Post form data, rename files, JS code completed
    Finished JS code, posted dorm data to register, renamed files, variables, images and classes.

---
+++ scripts/experienceForm.js
@@ -0,0 +1,158 @@
+// second form script
+const REDBERRY_URL = "https://chess-tournament-api.devtest.ge";
+
+// experience-level variables
+let selectMenu = document.querySelector('.selectMenu');
+let selectBtn = document.querySelector('.selectBtn');
+let selectBtnText = document.querySelector('.selectBtnText');
+let arrowDown = document.querySelector('.arrowDown');
+let optionsParent = document.querySelectorAll('.option');
+let options = document.querySelector('.options');
+let form = document.getElementById('secondForm');
+let check = document.querySelector('.secondBlock');
+let radioBtns = document.getElementsByName('yes/no') ;
+// character-id variables
+let secondselectBtnText = document.querySelector('.secondSelectBtnText');
+let secondSelectMenu = document.querySelector('.secondSelectMenu');
+let secondSelectBtn = document.querySelector('.secondSelectBtn');
+let secondArrowDown = document.querySelector('.secondArrowDown');
+let secondOptions = document.querySelector('.secondOptions');
+let total = document.querySelector('.total');
+let nextBtn = document.querySelector('#secondNextBtn');
+
+
+// After refresh, the form is being checked
+if(localStorage.getItem('experience_level') != null){
+    selectBtnText.innerText = localStorage.getItem('experience_level');
+} else {
+    selectBtnText.innerText == 'level of knowledge';
+}
+if(localStorage.getItem('character_idText') != null){
+    secondselectBtnText.innerText = localStorage.getItem('character_idText');
+} else{
+    secondselectBtnText.innerText == 'Choose your character';
+}
+
+if(localStorage.getItem('already_participated0') === 'true'){
+    radioBtns[0].checked = true;
+    radioBtns[1].checked = false;
+} else{
+    radioBtns[0].checked = false;
+    radioBtns[1].checked = true;
+}
+if(localStorage.getItem('already_participated0') == null){
+    radioBtns[1].checked = false;   
+    radioBtns[0].checked = false;
+}
+radioBtns[0].addEventListener('click', () => {
+    localStorage.setItem('already_participated0', radioBtns[0].checked);
+    localStorage.setItem('already_participated1', radioBtns[1].checked);
+})
+radioBtns[1].addEventListener('click', () => {
+    localStorage.setItem('already_participated0', radioBtns[0].checked);
+    localStorage.setItem('already_participated1', radioBtns[1].checked);
+})
+
+// Form submit action and send formdata
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    let isValid = true;
+    if(selectBtnText.innerText == 'level of knowledge' || secondselectBtnText.innerText == 'Choose your character'){
+        isValid = false;
+    } 
+    if(!radioBtns[0].checked && !radioBtns[1].checked){
+        isValid = false;
+    }
+    if(isValid){
+        let formData = {
+            name: localStorage.getItem('name'),
+            email: localStorage.getItem('email'),
+            phone: localStorage.getItem('phone'),
+            date: localStorage.getItem('date'),
+            experience_level: localStorage.getItem('experience_level'),
+            character_id: localStorage.getItem('character_id'),
+            already_participated: radioBtns[0].checked
+        };
+        console.log(formData);
+        fetch(`${REDBERRY_URL}/api/register`, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formData)
+          }).then(response => {
+                console.log(response);
+                localStorage.clear();
+                location.href ='onboardingCompleted.html';
+          })
+    }
+});
+
+// select input functional
+optionsParent.forEach((select) =>{
+    select.addEventListener('click', () =>{
+        let text = select.innerText;
+        selectBtnText.innerText = select.innerText;
+        localStorage.setItem('experience_level', selectBtnText.innerText);
+        options.classList.toggle('active');
+        arrowDown.classList.toggle('rotate2');
+        arrowDown.classList.toggle('rotate');
+        check.classList.add('success');
+    })
+})
+selectBtn.addEventListener('click', () =>{
+    options.classList.toggle('active');
+    arrowDown.classList.toggle('rotate2');
+    arrowDown.classList.toggle('rotate');
+})
+secondSelectBtn.addEventListener('click', () =>{
+    secondOptions.classList.toggle('active');
+    secondArrowDown.classList.toggle('rotate2');
+    secondArrowDown.classList.toggle('rotate');
+})
+
+// Get information from API
+const Http = new XMLHttpRequest();
+const url=  'https://chess-tournament-api.devtest.ge/api/grandmasters';
+Http.open("GET", url);
+Http.send();
+Http.onreadystatechange = (e) => {
+    if (Http.readyState == 4 && Http.status == 200) {
+        let reques = JSON.parse(Http.responseText)
+        for(let i =0 ; i < reques.length; i++){
+            let li = document.createElement('li');
+            let img = document.createElement('img');
+            li.classList.add('secondOption','flex');
+            li.innerHTML = reques[i].name;
+            li.setAttribute('id',reques[i].id);
+            img.classList.add('masterPhoto');
+            img.setAttribute('alt','character');
+            img.setAttribute('src','https://chess-tournament-api.devtest.ge'+ reques[i].image);
+            total.innerText = '(Total'+(i + 1)+')';
+            secondOptions.appendChild(li);
+            li.appendChild(img);
+        }
+        let other = document.createElement('li')
+        other.classList.add('secondOption','flex');
+        other.innerHTML = 'Other <img class="masterPhoto" alt="def" src="https://www.roomlets.rentals/templates/default_theme/img/person.jpg"     >'
+        secondOptions.appendChild(other);
+        secondOptions.childNodes.forEach((child) =>{
+            child.addEventListener('click', () =>{
+                let text = child.innerText;
+                secondselectBtnText.innerText = child.innerText;
+                secondselectBtnText.setAttribute('id', child.getAttribute('id'));
+                localStorage.setItem('character_idText', secondselectBtnText.innerText);
+                localStorage.setItem('character_id', secondselectBtnText.getAttribute('id'));
+                secondOptions.classList.toggle('active');
+                secondArrowDown.classList.toggle('rotate2');
+                secondArrowDown.classList.toggle('rotate');
+            })
+        })
+    }
+}
+
+
+
+
+
--- scripts/personalForm.js
+++ scripts/personalForm.js
@@ -12,52 +12,92 @@ let closeBtn = document.getElementById('alertX');
 let nextBtn = document.getElementById('nextBtn');
 let doublecheck = document.getElementById('doubleCheck');
 
-
+// local storage
 inputs[0].value = localStorage.getItem('name');
 inputs[1].value = localStorage.getItem('email');
 inputs[2].value = localStorage.getItem('phone');
 inputs[3].value = localStorage.getItem('date');
 
-// //first form script
+// After refresh, the form is being checked
+if(checks[0].classList.contains('active') && checks[1].classList.contains('active') && checks[2].classList.contains('active') && checks[3].classList.contains('active')){
+    doublecheck.classList.add('active');
+    box.innerText= '';
+}    
+if(localStorage.getItem('name') != null){
+    if(!localStorage.getItem('name') === '' || localStorage.getItem('name').length < 2){
+        setError(name,0,'Please enter name');
+        alertBox.classList.remove('active');
+        box.innerText= '1';
+        box.classList.add('success');
+    }  else{
+        setSuccess(name,0);
+    }
+}   
+if(localStorage.getItem('email') != null){ 
+    if(localStorage.getItem('email') != '' && localStorage.getItem('email').includes('@redberry.ge')){
+        setSuccess(email,1);  
+    } else{
+        setError(email,1,'Please enter email address');
+        alertBox.classList.remove('active');
+        box.innerText= '1';
+        box.classList.add('success');
+    }
+}
+if(localStorage.getItem('phone') != null){
+    if(localStorage.getItem('phone') != '' && !isNaN(localStorage.getItem('phone')) && localStorage.getItem('phone').length == 9){
+        setSuccess(phone,2);
+    } else {
+        setError(phone,2,'Length Must Be 9 Numbers Long');
+        alertBox.classList.remove('active');
+        box.innerText= '1';
+        box.classList.add('success');
+    }
+}
+if(localStorage.getItem('date') != null){
+    if(localStorage.getItem('date') === ''){
+        setError(date,3,'Please enter date of birth');
+        alertBox.classList.remove('active');
+        box.innerText= '1';
+        box.classList.add('success');
+    } else{
+        setSuccess(date,3);
+    }
+}
+if(checks[0].classList.contains('active') && checks[1].classList.contains('active') && checks[2].classList.contains('active') && checks[3].classList.contains('active')){
+    doublecheck.classList.add('active');
+    box.innerText= '';
+    box.classList.add('success');
+}else{
+    doublecheck.classList.remove('active');
+}
+
+// //first form submit action
 let isValidEmail = email => {
     let re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
 }
 form.addEventListener('submit', e => {
     e.preventDefault();
-    let isValid = true
-
-    let formData = {
-        name: e.target[0].value,
-        email: e.target[1].value,
-        phone: e.target[2].value,
-        date: e.target[3].value
-    }
-    console.log(formData);
+    let isValid = true;
     if(!checks[0].classList.contains('active')){
-        isValid = false
+        isValid = false;
     }
     if(!checks[1].classList.contains('active')){
-        isValid = false
+        isValid = false;
     }
     if(!checks[2].classList.contains('active')){
-        isValid = false
+        isValid = false;
     }
     if(!checks[3].classList.contains('active')){
-        isValid = false
+        isValid = false;
     }
     if(isValid){
-        location.href ='secondForm.html';
+        location.href ='experienceInfo.html';
     }
 }
-    
-
-
 );
-closeBtn.addEventListener('click',() =>{
-    alertBox.classList.remove('active');
-})
 
+// Inputs validation
 inputs[0].addEventListener('blur', e => {
     let alertText = document.getElementById('alert');
     let nameValue = name.value.trim();
@@ -69,9 +109,12 @@ inputs[0].addEventListener('blur', e => {
         setSuccess(name,0);
     }
     box.classList.add('success');
-    if((checks[2], checks[1], checks[2], checks[3]).classList.contains('active')){
+    if(checks[0].classList.contains('active') && checks[1].classList.contains('active') && checks[2].classList.contains('active') && checks[3].classList.contains('active')){
         doublecheck.classList.add('active');
-        box.innerText= ''
+        box.innerText= '';
+    } else{
+        doublecheck.classList.remove('active');
+        box.innerText= '1';
     }
     localStorage.setItem('name', inputs[0].value);    
 });
@@ -88,9 +131,12 @@ inputs[1].addEventListener('blur', e => {
         setSuccess(email,1);
     }
     box.classList.add('success');
-    if((checks[2], checks[1], checks[2], checks[3]).classList.contains('active')){
+    if(checks[0].classList.contains('active') && checks[1].classList.contains('active') && checks[2].classList.contains('active') && checks[3].classList.contains('active')){
         doublecheck.classList.add('active');
-        box.innerText= ''
+        box.innerText= '';
+    }else{
+        doublecheck.classList.remove('active');
+        box.innerText= '1';
     }
     localStorage.setItem('email', inputs[1].value);
 });
@@ -106,9 +152,12 @@ inputs[2].addEventListener('blur', e => {
         setSuccess(phone,2);
     }
     box.classList.add('success')
-    if((checks[2], checks[1], checks[2], checks[3]).classList.contains('active')){
+    if(checks[0].classList.contains('active') && checks[1].classList.contains('active') && checks[2].classList.contains('active') && checks[3].classList.contains('active')){
         doublecheck.classList.add('active');
-        box.innerText= ''
+        box.innerText= '';
+    }else{
+        doublecheck.classList.remove('active');
+        box.innerText= '1';
     }
     localStorage.setItem('phone', inputs[2].value);
 });
@@ -120,13 +169,27 @@ inputs[3].addEventListener('blur', e => {
         setSuccess(date,3);
     }
     box.classList.add('success');
-    if((checks[2], checks[1], checks[2], checks[3]).classList.contains('active')){
+    if(checks[0].classList.contains('active') && checks[1].classList.contains('active') && checks[2].classList.contains('active') && checks[3].classList.contains('active')){
         doublecheck.classList.add('active');
-        box.innerText= ''
+        box.innerText= '';
+    }else{
+        doublecheck.classList.remove('active');
+        box.innerText= '1';
     }
     localStorage.setItem('date', inputs[3].value);
 });
+nextBtn.addEventListener('click',() =>{
+    if(!doublecheck.classList.contains('active')){
+        alertBox.classList.add('active');
+        alertText.innerHTML = 'Fill Inputs Correctly';
+        box.innerText= '1';
+    }
+})
+closeBtn.addEventListener('click',() =>{
+    alertBox.classList.remove('active');
+})
 
+// functions declaration
 function setError(element,num,message){
     element.classList.add('error');
     element.classList.remove('success');
@@ -134,16 +197,10 @@ function setError(element,num,message){
     alertBox.classList.add('active');
     alertText.innerHTML = message;
 }
-
 function setSuccess(element,num){
     element.classList.remove('error');
     element.classList.add('success');
     checks[num].classList.add('active');
     alertBox.classList.remove('active');
 }
-nextBtn.addEventListener('click',() =>{
-    if(!nextBtn.hasAttribute('onclick')){
-        alertBox.classList.add('active');
-        alertText.innerHTML = 'Fill Inputs Correctly'
-    }
-})
+
